1393f188532260eb3adefa0fb8df626c16389fd2
Michael Schiller <38842472+MichaelSSchiller@users.noreply.github.com>
2024-01-12 20:00:25 -0500

    MS-04 add tests (#4)

---
+++ app/routes/_index.test.tsx
@@ -0,0 +1,47 @@
+import { action } from "./_index";
+
+// All valid credit card numbers
+const valid1 = "4539677908016808";
+const valid2 = "5535766768751439";
+const valid3 = "371612019985236";
+const valid4 = "6011144340682905";
+const valid5 = "4539404967869666";
+
+// All invalid credit card numbers
+const invalid1 = "4532778771091795";
+const invalid2 = "5795593392134643";
+const invalid3 = "375796084459914";
+const invalid4 = "6011127961777935";
+const invalid5 = "5382019772883854";
+
+const batch: [string, boolean][] = [
+  [valid1, true],
+  [valid2, true],
+  [valid3, true],
+  [valid4, true],
+  [valid5, true],
+  [invalid1, false],
+  [invalid2, false],
+  [invalid3, false],
+  [invalid4, false],
+  [invalid5, false],
+];
+
+describe("Path Action", () => {
+  test.each(batch)(
+    "card number %p should validate to %p",
+    async (cardNumber, isValid) => {
+      const body = new URLSearchParams({
+        cardNumber,
+      });
+      const request = new Request("http://app.com/path", {
+        method: "POST",
+        body,
+      });
+
+      const response = await action({ request, params: {}, context: {} });
+      const data = await response.json();
+      expect(data).toEqual({ isValid });
+    },
+  );
+});
